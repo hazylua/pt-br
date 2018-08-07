4bc94c1e839a25b2ae55d740a44f5a69dbce73e6
yb00 <yagobraz98@gmail.com>
2018-08-07 16:36:58 -0300

    Alterações.

--- c/pilha_encadeada_descritor/pilha.h
+++ c/pilha_encadeada_descritor/pilha.h
@@ -18,26 +18,21 @@ typedef struct pilha
     int qtd;
 } PILHA;
 
-// Criação e liberação da estrutura de dados
 PILHA *criar_pilha ();
 void liberar_pilha (PILHA *);
 
-// Verificação e exibição de elementos da estrutura de dados
 int verificar_tam (PILHA *);
 int verificar_preenchida (PILHA *);
 int verificar_vazia (PILHA *);
 int mostrar_elementos (PILHA *);
 int mostrar_topo (PILHA *);
 
-// Remoção e inserção de elementos na estrutura de dados
 int inserir_final (PILHA *);
 int remover_final (PILHA *);
 int esvaziar_pilha (PILHA *);
 
-// Validação de parênteses
 int validar_parenteses (PILHA *);
 
-// Alternativa a função fflush (stdin)
 void clean_stdin ();
 
 #endif
