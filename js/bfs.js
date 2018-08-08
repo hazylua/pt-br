b47c8feaa2a295e86216fc4995a7d8a45f297ad8
yb00 <yagobraz98@gmail.com>
2018-08-08 01:40:54 -0300

    Alterações.

--- js/bfs.js
+++ js/bfs.js
@@ -1,15 +1,57 @@
+// adj represente o nó adjacente, sendo que o primeiro número dos pares deste vetor represente o nó adjacente e o segundo número representa a distância entre esses nós. 
 var nos = [
     {
-        //...
+        // Vértice 1
+        nome: 'a',
+        adj: [ [ 2,4 ], [ 8,8 ] ],
+        cor: "branco"
     }, {
-        //...
-    },
+        // Vértice 2
+        nome: 'b',
+        adj: [ [1,4 ], [ 8,11 ], [ 3,8 ] ],
+        cor: "branco"
+    }, {
+        // Vértice 3
+        nome: 'c',
+        adj: [ [ 2,8 ], [ 9,2 ], [ 6,4 ], [ 4,7 ] ],
+        cor: "branco"
+    }, {
+        // Vértice 4
+        nome: 'd',
+        adj: [ [ 3,7 ], [ 6,14 ], [ 5,9 ] ],
+        cor: "branco"
+    }, {
+        // Vértice 5
+        nome: 'e',
+        adj: [ [ 4,9 ], [ 6,10 ] ],
+        cor: "branco"
+    }, {
+        // Vértice 6
+        nome: 'f',
+        adj: [ [ 5,10 ], [ 4,14 ], [ 3,4 ], [ 7,2 ] ],
+        cor: "branco"
+    }, {
+        // Vértice 7
+        nome: 'g',
+        adj: [ [ 6,2 ], [ 9,1 ], [ 8,1 ] ],
+        cor: "branco"
+    }, {
+        // Vértice 8
+        nome: 'h',
+        adj: [ [ 1,8 ], [ 2,11 ], [ 9,7 ], [ 7,1 ] ],
+        cor: "branco"
+    }, {
+        // Vértice 9
+        nome: 'i',
+        adj: [ [ 3,2 ], [ 8,7 ], [ 7,1 ] ],
+        cor: "branco"
+    }
 ]
 
-function bfs ( start ) {
-    var lista = [ start ];
+function bfs( s ) {
+    var lista = [ s ];
     
-    nos[ start ].cor = "branco";
+    nos[ s ].cor = "branco";
 
     while ( lista.length > 0 ) {
         var u = lista.shift ();
---
+++ js/test.js
@@ -0,0 +1,90 @@
+var nos = [
+    {
+        // Início
+        nome: "inicio"
+    }, {
+        // Vértice 1
+        nome: 'a',
+        adj: [ 2, 8 ],
+        cor: "branco",
+        d: null,
+        pi: null
+    }, {
+        // Vértice 2
+        nome: 'b',
+        adj: [ 1, 8, 3 ],
+        cor: "branco",
+        d: null,
+        pi: null
+    }, {
+        // Vértice 3
+        nome: 'c',
+        adj: [ 2, 9, 6, 4 ],
+        cor: "branco",
+        d: null,
+        pi: null
+    }, {
+        // Vértice 4
+        nome: 'd',
+        adj: [ 3, 6, 5 ],
+        cor: "branco",
+        d: null,
+        pi: null
+    }, {
+        // Vértice 5
+        nome: 'e',
+        adj: [ 4, 6 ],
+        cor: "branco",
+        d: null,
+        pi: null
+    }, {
+        // Vértice 6
+        nome: 'f',
+        adj: [ 5, 3, 3, 7 ],
+        cor: "branco",
+        d: null,
+        pi: null
+    }, {
+        // Vértice 7
+        nome: 'g',
+        adj: [ 6, 9, 8 ],
+        cor: "branco",
+        d: null,
+        pi: null
+    }, {
+        // Vértice 8
+        nome: 'h',
+        adj: [ 1, 2, 9, 7 ],
+        cor: "branco",
+        d: null,
+        pi: null
+    }, {
+        // Vértice 9
+        nome: 'i',
+        adj: [ 3, 8, 7 ],
+        cor: "branco",
+        d: null,
+        pi: null
+    }
+]
+
+function bfs( s ) {
+    var lista = [ s ];
+    
+    nos[ s ].d = 0;
+
+    while( lista.length > 0 ) {
+        var u = lista.shift ();
+        nos[ u ].adj.forEach( function( v ) { 
+            if( nos[ v ].cor == "branco" ) {
+                nos[ v ].cor = "cinza";
+                nos[ v ].d = nos[ u ].d + 1;
+                nos[ v ].pi = u; 
+                lista.push ( v );
+            }
+        } );
+        nos[ u ].cor = "preto";
+    }
+}
+
+bfs ( 1 );
