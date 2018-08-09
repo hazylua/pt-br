869cf9465cfd584c7bc645ce90a85bea3b33a100
Aluno <aluno@exemplo.com.br>
2018-08-09 08:37:59 -0300

    Correções.

--- js/bfs.js
+++ js/bfs.js
@@ -1,68 +1,90 @@
-// adj represente o nó adjacente, sendo que o primeiro número dos pares deste vetor represente o nó adjacente e o segundo número representa a distância entre esses nós. 
 var nos = [
     {
+        // Cabeça
+        nome: null,
+    }, {
         // Vértice 1
         nome: 'a',
-        adj: [ [ 2,4 ], [ 8,8 ] ],
-        cor: "branco"
+        adj: [ 2, 8 ],
+        cor: "branco",
+        d: null,
+        pi: null
     }, {
         // Vértice 2
         nome: 'b',
-        adj: [ [1,4 ], [ 8,11 ], [ 3,8 ] ],
-        cor: "branco"
+        adj: [ 1, 8, 3 ],
+        cor: "branco",
+        d: null,
+        pi: null
     }, {
         // Vértice 3
         nome: 'c',
-        adj: [ [ 2,8 ], [ 9,2 ], [ 6,4 ], [ 4,7 ] ],
-        cor: "branco"
+        adj: [ 2, 9, 6, 4 ],
+        cor: "branco",
+        d: null,
+        pi: null
     }, {
         // Vértice 4
         nome: 'd',
-        adj: [ [ 3,7 ], [ 6,14 ], [ 5,9 ] ],
-        cor: "branco"
+        adj: [ 3, 6, 5 ],
+        cor: "branco",
+        d: null,
+        pi: null
     }, {
         // Vértice 5
         nome: 'e',
-        adj: [ [ 4,9 ], [ 6,10 ] ],
-        cor: "branco"
+        adj: [ 4, 6 ],
+        cor: "branco",
+        d: null,
+        pi: null
     }, {
         // Vértice 6
         nome: 'f',
-        adj: [ [ 5,10 ], [ 4,14 ], [ 3,4 ], [ 7,2 ] ],
-        cor: "branco"
+        adj: [ 5, 3, 3, 7 ],
+        cor: "branco",
+        d: null,
+        pi: null
     }, {
         // Vértice 7
         nome: 'g',
-        adj: [ [ 6,2 ], [ 9,1 ], [ 8,1 ] ],
-        cor: "branco"
+        adj: [ 6, 9, 8 ],
+        cor: "branco",
+        d: null,
+        pi: null
     }, {
         // Vértice 8
         nome: 'h',
-        adj: [ [ 1,8 ], [ 2,11 ], [ 9,7 ], [ 7,1 ] ],
-        cor: "branco"
+        adj: [ 1, 2, 9, 7 ],
+        cor: "branco",
+        d: null,
+        pi: null
     }, {
         // Vértice 9
         nome: 'i',
-        adj: [ [ 3,2 ], [ 8,7 ], [ 7,1 ] ],
-        cor: "branco"
+        adj: [ 3, 8, 7 ],
+        cor: "branco",
+        d: null,
+        pi: null
     }
 ]
 
 function bfs( s ) {
     var lista = [ s ];
     
-    nos[ s ].cor = "branco";
+    nos[ s ].d = 0;
 
-    while ( lista.length > 0 ) {
+    while( lista.length > 0 ) {
         var u = lista.shift ();
-        no [ u ].adj.forEach ( function ( v ) {
-            if ( no[ v ].cor == "branco" ) {
-                nos [ v ].cor = "cinza";
+        nos[ u ].adj.forEach( function( v ) { 
+            if( nos[ v ].cor == "branco" ) {
+                nos[ v ].cor = "cinza";
+                nos[ v ].d = nos[ u ].d + 1;
+                nos[ v ].pi = u; 
                 lista.push ( v );
             }
         } );
-        nos [ v ].cor == "preto";
+        nos[ u ].cor = "preto";
     }
 }
 
-bfs ( 0 );
+bfs ( 1 );
--- js/dfs.js
+++ js/dfs.js
@@ -1,26 +1,110 @@
-const nos = [
+var nos = [
     {
-        //...
+        // Cabeça
+        nome: null,
     }, {
-        //...
-    },
+        // Vértice 1
+        nome: 'a',
+        adj: [ 2, 8 ],
+        cor: "branco",
+        d: null,
+        pi: null,
+        f: null,
+    }, {
+        // Vértice 2
+        nome: 'b',
+        adj: [ 1, 8, 3 ],
+        cor: "branco",
+        d: null,
+        pi: null,
+        f: null,
+    }, {
+        // Vértice 3
+        nome: 'c',
+        adj: [ 2, 9, 6, 4 ],
+        cor: "branco",
+        d: null,
+        pi: null,
+        f: null,
+    }, {
+        // Vértice 4
+        nome: 'd',
+        adj: [ 3, 6, 5 ],
+        cor: "branco",
+        d: null,
+        pi: null,
+        f: null,
+    }, {
+        // Vértice 5
+        nome: 'e',
+        adj: [ 4, 6 ],
+        cor: "branco",
+        d: null,
+        pi: null,
+        f: null,
+    }, {
+        // Vértice 6
+        nome: 'f',
+        adj: [ 5, 3, 3, 7 ],
+        cor: "branco",
+        d: null,
+        pi: null,
+        f: null,
+    }, {
+        // Vértice 7
+        nome: 'g',
+        adj: [ 6, 9, 8 ],
+        cor: "branco",
+        d: null,
+        pi: null,
+        f: null,
+    }, {
+        // Vértice 8
+        nome: 'h',
+        adj: [ 1, 2, 9, 7 ],
+        cor: "branco",
+        d: null,
+        pi: null,
+        f: null,
+    }, {
+        // Vértice 9
+        nome: 'i',
+        adj: [ 3, 8, 7 ],
+        cor: "branco",
+        d: null,
+        pi: null,
+        f: null,
+    }
 ]
 
-const dfs = s => {
-    const lista = [ s ];
-    
-    nos[ s ].cor = "branco";
+var tempo = 0;
 
-    while ( lista.length ) {
-        const u = lista.pop ();
+function dfs( s ) {
+    const lista = [ s ];
+
+    while( lista.length > 0 ) {
+        var u = lista.pop();
         
-        nos[ u ].adj.forEach ( v => {
-            if ( nos[ v ].cor == "branco" )
-                lista.push ( v );
-            nos[ v ].cor = "cinza";
+        nos[ u ].adj.forEach( function ( v ) {
+            if( nos[ v ].cor == "branco" )
+                dfs_visit( nos[ v ] );
         } );
-        nos[ u ].cor = "preto";
     }
 }
 
-dfs ( 0 );
+function dfs_visit( u ) {
+    tempo += 1;
+    u.d = tempo;
+    u.cor = "cinza";
+    u.adj.forEach( function( v ) { 
+        if( nos[ v ].cor == "branco" ) {
+            nos[ v ].pi = u;
+            dfs_visit( nos[ v ] );
+        }
+    } );
+    u.cor = "preto";
+    tempo += 1;
+    u.f = tempo;
+}
+
+dfs( 1 );
