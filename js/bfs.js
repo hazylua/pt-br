d9c6436944d7741f373d00fb7f916d775c504e8d
yb00 <yagobraz98@gmail.com>
2018-08-14 17:53:32 -0300

    Alterações.

--- js/bfs.js
+++ js/bfs.js
@@ -1,112 +1,90 @@
 var nos = [
     {
         // Cabeça
-        nome: null
+        nome: null,
     }, {
         // Vértice 1
         nome: 'a',
         adj: [ 2, 8 ],
         cor: "branco",
         d: null,
-        pi: null,
-        f: null,
+        pi: null
     }, {
         // Vértice 2
         nome: 'b',
         adj: [ 1, 8, 3 ],
         cor: "branco",
         d: null,
-        pi: null,
-        f: null,
+        pi: null
     }, {
         // Vértice 3
         nome: 'c',
         adj: [ 2, 9, 6, 4 ],
         cor: "branco",
         d: null,
-        pi: null,
-        f: null,
+        pi: null
     }, {
         // Vértice 4
         nome: 'd',
         adj: [ 3, 6, 5 ],
         cor: "branco",
         d: null,
-        pi: null,
-        f: null,
+        pi: null
     }, {
         // Vértice 5
         nome: 'e',
         adj: [ 4, 6 ],
         cor: "branco",
         d: null,
-        pi: null,
-        f: null,
+        pi: null
     }, {
         // Vértice 6
         nome: 'f',
         adj: [ 5, 3, 3, 7 ],
         cor: "branco",
         d: null,
-        pi: null,
-        f: null,
+        pi: null
     }, {
         // Vértice 7
         nome: 'g',
         adj: [ 6, 9, 8 ],
         cor: "branco",
         d: null,
-        pi: null,
-        f: null,
+        pi: null
     }, {
         // Vértice 8
         nome: 'h',
         adj: [ 1, 2, 9, 7 ],
         cor: "branco",
         d: null,
-        pi: null,
-        f: null,
+        pi: null
     }, {
         // Vértice 9
         nome: 'i',
         adj: [ 3, 8, 7 ],
         cor: "branco",
         d: null,
-        pi: null,
-        f: null,
+        pi: null
     }
 ]
-var tempo = 0;
-function dfs ( s ) {
+
+function bfs( s ) {
     var lista = [ s ];
     
-    nos[ s ].pi = 0;
+    nos[ s ].d = 0;
 
     while( lista.length > 0 ) {
         var u = lista.shift ();
         nos[ u ].adj.forEach( function( v ) { 
             if( nos[ v ].cor == "branco" ) {
-                dfs_visit( v );
+                nos[ v ].cor = "cinza";
+                nos[ v ].d = nos[ u ].d + 1;
+                nos[ v ].pi = u; 
+                lista.push ( v );
             }
         } );
         nos[ u ].cor = "preto";
     }
 }
 
-function dfs_visit( u ) {
-    nos[ u ].cor = "cinza";
-    tempo = tempo + 1;
-    nos[ u ].d = tempo;
-    nos[ u ].adj.forEach( function ( v ) { 
-        if(nos[ v ].cor == "branco") {
-            nos[ v ].pi = u;
-            dfs_visit( nos[ v ] )
-        }
-    } );
-    nos[ u ].cor = "preto";
-    tempo = tempo +1;
-    nos[ u ].f = tempo;
-}
-
-// Começa no vértice A (1)
-dfs ( 1 );
+bfs ( 1 );
--- js/dfs.js
+++ js/dfs.js
@@ -1,92 +1,110 @@
 var nos = [
     {
         // Cabeça
-        nome: null
+        nome: null,
     }, {
         // Vértice 1
         nome: 'a',
         adj: [ 2, 8 ],
         cor: "branco",
         d: null,
-        pi: null
+        pi: null,
+        f: null,
     }, {
         // Vértice 2
         nome: 'b',
         adj: [ 1, 8, 3 ],
         cor: "branco",
         d: null,
-        pi: null
+        pi: null,
+        f: null,
     }, {
         // Vértice 3
         nome: 'c',
         adj: [ 2, 9, 6, 4 ],
         cor: "branco",
         d: null,
-        pi: null
+        pi: null,
+        f: null,
     }, {
         // Vértice 4
         nome: 'd',
         adj: [ 3, 6, 5 ],
         cor: "branco",
         d: null,
-        pi: null
+        pi: null,
+        f: null,
     }, {
         // Vértice 5
         nome: 'e',
         adj: [ 4, 6 ],
         cor: "branco",
         d: null,
-        pi: null
+        pi: null,
+        f: null,
     }, {
         // Vértice 6
         nome: 'f',
         adj: [ 5, 3, 3, 7 ],
         cor: "branco",
         d: null,
-        pi: null
+        pi: null,
+        f: null,
     }, {
         // Vértice 7
         nome: 'g',
         adj: [ 6, 9, 8 ],
         cor: "branco",
         d: null,
-        pi: null
+        pi: null,
+        f: null,
     }, {
         // Vértice 8
         nome: 'h',
         adj: [ 1, 2, 9, 7 ],
         cor: "branco",
         d: null,
-        pi: null
+        pi: null,
+        f: null,
     }, {
         // Vértice 9
         nome: 'i',
         adj: [ 3, 8, 7 ],
         cor: "branco",
         d: null,
-        pi: null
+        pi: null,
+        f: null,
     }
 ]
 
-function bfs( s ) {
-    var lista = [ s ];
-    
-    nos[ s ].d = 0;
+var tempo = 0;
+
+function dfs( s ) {
+    const lista = [ s ];
 
     while( lista.length > 0 ) {
-        var u = lista.shift ();
-        nos[ u ].adj.forEach( function( v ) { 
-            if( nos[ v ].cor == "branco" ) {
-                nos[ v ].cor = "cinza";
-                nos[ v ].d = nos[ u ].d + 1;
-                nos[ v ].pi = u; 
-                lista.push ( v );
-            }
+        var u = lista.pop();
+        
+        nos[ u ].adj.forEach( function ( v ) {
+            if( nos[ v ].cor == "branco" )
+                dfs_visit( nos[ v ] );
         } );
-        nos[ u ].cor = "preto";
-        nos.forEach( function( x ) { console.log( x.d )} );
     }
 }
 
-// Começa no vértice A (1)
-bfs ( 1 );
+function dfs_visit( u ) {
+    tempo += 1;
+    u.d = tempo;
+    u.cor = "cinza";
+    u.adj.forEach( function( v ) { 
+        if( nos[ v ].cor == "branco" ) {
+            nos[ v ].pi = u;
+            dfs_visit( nos[ v ] );
+        }
+    } );
+    u.cor = "preto";
+    tempo += 1;
+    u.f = tempo;
+}
+
+dfs( 1 );
