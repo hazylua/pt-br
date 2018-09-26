61eb74c5c341b46d6b197d5b78000d983b8e50a2
Aluno <aluno@exemplo.com.br>
2018-09-26 10:02:39 -0300

    Alterações.

---
+++ c/karatsuba/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+unsigned long long int multiplica( int, int, int );
+
+int main() {
+
+    int u = 10000000, v = 7, n = 8;
+    printf( "u = %d, v = %d, n = %d\n", u, v, n );
+    printf("%llu\n", multiplica(u, v, n));
+    return 0;
+}
+
+unsigned long long int multiplica( int u, int v, int n ) {
+
+    if( n == 1 )    
+        return (u * v);
+    else {
+        int k = (n / 2);
+        int q = pow( 10, k );
+
+        int a = (u / q);
+        int b = (u % q);
+
+        int c = (v / q);
+        int d = (v % q);
+
+        int ac = multiplica( a, c, k );
+        int bd = multiplica( b, d, k );
+        int ad = multiplica( a, d, k );
+        int bc = multiplica( b, c, k );
+
+        unsigned long long int x = ac * pow(10, 2 * k) + (ad + bc) * pow(10, k) + bd;
+        
+        return x;
+    }
+}
