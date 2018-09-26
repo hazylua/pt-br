6bc333018bc5e3eab6103b0ad7442eaf49f2f20f
Yago <yagobraz98@gmail.com>
2018-09-26 10:05:25 -0300

    Alterações.

--- config_git_local.sh
+++ config_git_local.sh
@@ -2,7 +2,7 @@
 read -p 'Github user name: ' username
 read -p 'Github user email: ' usermail
 echo
-git config user.name $uservar
+git config user.name $username
 git config user.email $usermail
 echo "Exibição das configurações do repositório: "
 cat .git/config 
