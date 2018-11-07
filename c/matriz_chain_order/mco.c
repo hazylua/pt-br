9cc6fca974488e344f6de04d77cd1c7dec9ded9a
yb00 <yagobraz98@gmail.com>
2018-11-07 07:11:22 -0200

    Alterações.

--- c/matriz_chain_order/mco.c
+++ c/matriz_chain_order/mco.c
@@ -4,11 +4,12 @@
 
 int matrix_chain_order( int *, int );
 
+int printparents( int (*)[], int, int );
+
 int main()
 {   
-    int A[] = {1, 2, 3, 4};
+    int A[] = {10, 30, 5, 10};
     int n = sizeof(A)/sizeof(A[0]);
-    printf( "tam = %d\n", n );
     printf( "res=%d\n", matrix_chain_order( A, n ) );
 
     return 0;
@@ -18,6 +19,7 @@ int matrix_chain_order( int A[], int n )
 {
     //Linha 0 e coluna 0 não são usadas.
     int m[n][n];
+    int s[n][n];
 
     int i, j, k, L, q;
 
@@ -36,10 +38,38 @@ int matrix_chain_order( int A[], int n )
                 if( q < m[i][j] )
                 {
                     m[i][j] = q;
+                    s[i][j] = k;
                 }
             }
         }    
     }
 
+    printparents( s, 1, n-1 );
+
     return m[1][n - 1];
 }
+
+int printparents( int s[][4], int i, int j )
+{
+    if( i == j )
+    {
+        printf( " A_%d ", i );
+    }
+    else
+    {
+        printf( "(" );
+        printparents( s, i, s[i][j] );
+        printparents( s, s[i][j] + 1, j );
+        printf( ")" );
+    }
+}
+
+
+
+
+
+
+
+
+
+
